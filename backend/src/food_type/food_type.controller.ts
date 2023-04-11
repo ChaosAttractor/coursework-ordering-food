1e85e968b689ec6064a8f428c8b69eef70659dea
ytuipt <soyjackthecuckold@gmail.com>
2023-04-11 12:52:06 +0300

    added guards

--- backend/src/food_type/food_type.controller.ts
+++ backend/src/food_type/food_type.controller.ts
@@ -7,31 +7,44 @@ import {
   Param,
   Delete,
   ParseIntPipe,
+  UseGuards,
 } from '@nestjs/common';
 import { FoodTypeService } from './food_type.service';
 import { CreateFoodTypeDto } from './dto/create-food_type.dto';
 import { UpdateFoodTypeDto } from './dto/update-food_type.dto';
 import { FoodType } from './entities/food_type.entity';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { Roles } from 'src/auth/roles/role.decorator';
+import { RoleList } from 'src/auth/roles/role.enum';
+import { Public } from 'src/auth/roles/public';
 
 @Controller('food-type')
 export class FoodTypeController {
   constructor(private readonly foodTypeService: FoodTypeService) {}
 
+  @UseGuards(JwtAuthGuard)
+  @Roles(RoleList.Admin)
   @Post()
   create(@Body() createFoodTypeDto: CreateFoodTypeDto): Promise<FoodType> {
     return this.foodTypeService.create(createFoodTypeDto);
   }
 
+  @UseGuards(JwtAuthGuard)
+  @Public()
   @Get()
   findAll(): Promise<FoodType[]> {
     return this.foodTypeService.findAll();
   }
 
+  @UseGuards(JwtAuthGuard)
+  @Public()
   @Get(':id')
   findOne(@Param('id', ParseIntPipe) id: number): Promise<FoodType> {
     return this.foodTypeService.findOne(id);
   }
 
+  @UseGuards(JwtAuthGuard)
+  @Roles(RoleList.Admin)
   @Patch(':id')
   update(
     @Param('id', ParseIntPipe) id: number,
@@ -40,6 +53,8 @@ export class FoodTypeController {
     return this.foodTypeService.update(id, updateFoodTypeDto);
   }
 
+  @UseGuards(JwtAuthGuard)
+  @Roles(RoleList.Admin)
   @Delete(':id')
   remove(@Param('id', ParseIntPipe) id: number): Promise<void> {
     return this.foodTypeService.remove(id);
--- backend/src/menu/menu.controller.ts
+++ backend/src/menu/menu.controller.ts
@@ -7,31 +7,44 @@ import {
   Param,
   Delete,
   ParseIntPipe,
+  UseGuards,
 } from '@nestjs/common';
 import { MenuService } from './menu.service';
 import { CreateMenuDto } from './dto/create-menu.dto';
 import { UpdateMenuDto } from './dto/update-menu.dto';
 import { Menu } from './entities/menu.entity';
+import { Roles } from 'src/auth/roles/role.decorator';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { RoleList } from 'src/auth/roles/role.enum';
+import { Public } from 'src/auth/roles/public';
 
 @Controller('menu')
 export class MenuController {
   constructor(private readonly menuService: MenuService) {}
 
+  @UseGuards(JwtAuthGuard)
+  @Roles(RoleList.Admin)
   @Post()
   create(@Body() createMenuDto: CreateMenuDto): Promise<Menu> {
     return this.menuService.create(createMenuDto);
   }
 
+  @UseGuards(JwtAuthGuard)
+  @Public()
   @Get()
   findAll(): Promise<Menu[]> {
     return this.menuService.findAll();
   }
 
+  @UseGuards(JwtAuthGuard)
+  @Public()
   @Get(':id')
   findOne(@Param('id', ParseIntPipe) id: number): Promise<Menu> {
     return this.menuService.findOne(id);
   }
 
+  @UseGuards(JwtAuthGuard)
+  @Roles(RoleList.Admin)
   @Patch(':id')
   update(
     @Param('id', ParseIntPipe) id: number,
@@ -40,6 +53,8 @@ export class MenuController {
     return this.menuService.update(id, updateMenuDto);
   }
 
+  @UseGuards(JwtAuthGuard)
+  @Roles(RoleList.Admin)
   @Delete(':id')
   remove(@Param('id', ParseIntPipe) id: number): Promise<void> {
     return this.menuService.remove(id);
