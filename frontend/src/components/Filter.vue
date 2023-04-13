9264b17f51c10b0be62492d4a574df7cab391d24
ytuipt <soyjackthecuckold@gmail.com>
2023-04-13 10:42:32 +0300

    filter.vue almost done

--- frontend/src/components/Filter.vue
+++ frontend/src/components/Filter.vue
@@ -1,14 +1,20 @@
 <template>
   <div class="flex gap-[30px] items-center">
     <p class="font-montserrat font-bold text-[24px] text-white">Фильтры</p>
-    <select v-model="selectedType">
-      <option disabled value="">Выберите тип</option>
-      <!-- option v-for через бэк -->
-    </select>
+    <v-select
+      :options="books"
+      :placeholder="selectedPlaceholder"
+      label="title"
+      class="w-[340px] bg-alt-white rounded-[60px] px-[10px] font-montserrat font-bold text-[20px]"
+    />
+
+    <!-- class="p-[5px] pt-[4px] rounded-[60px] font-montserrat text-[24px] font-bold bg-alt-white"
+    > -->
+    <!-- option v-for через бэк -->
     <div
-      class="flex w-[340px] h-[40px] justify-between items-center rounded-[60px] bg-alt-white"
+      class="flex w-[340px] h-[36px] justify-between items-center rounded-[60px] bg-alt-white"
     >
-      <p class="font-montserrat pl-[10px] font-bold text-[24px] text-black">
+      <p class="font-montserrat pl-[10px] font-bold text-[20px] text-black">
         По цене
       </p>
       <vue-slider
@@ -28,10 +34,13 @@
 <script>
 import VueSlider from "vue-slider-component";
 import "vue-slider-component/theme/default.css";
+import vSelect from "vue-select";
+import "vue-select/dist/vue-select.css";
 
 export default {
   components: {
     VueSlider,
+    vSelect,
   },
   data() {
     return {
@@ -39,6 +48,15 @@ export default {
       rangeValue: [0, 500],
       min: 0,
       max: 500,
+      selectedPlaceholder: "По типу",
+      books: [
+        { title: "Old Man's War" },
+        { title: "The Lock Artist" },
+        { title: "HTML5" },
+        { title: "Right Ho Jeeves" },
+        { title: "The Code of the Wooster" },
+        { title: "Thank You Jeeves" },
+      ],
     };
   },
   methods: {
@@ -50,3 +68,19 @@ export default {
   },
 };
 </script>
+
+<style>
+:root {
+  --vs-border-width: 0px;
+  --vs-dropdown-bg: #d9d9d9;
+  --font-size: 20px;
+}
+
+.vs__search {
+  font-size: 20px;
+}
+
+.vs_search {
+  height: 29px;
+}
+</style>
